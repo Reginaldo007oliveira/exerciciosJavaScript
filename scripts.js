09312c09a01e2d78c50c3e1797519147c097c729
Atacadoweb <atacadowebonline@gmail.com>
2021-06-29 12:23:03 -0300

    Passa uma div do html para o Console

--- scripts.js
+++ scripts.js
@@ -1,7 +1,2 @@
-//convert Celsius to Fahrenheit
-
-function convertToFahrenheit(value){
-    return value * 1.8 + 32
-}
-let result = convertToFahrenheit(380)
-console.log(`O valor em Fahrenheit é ${result}`)
+const listContainer = document.querySelector('[data-lists]')
+console.log(listContainer)

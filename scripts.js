72e3c9bcd77c684ea872dc4960ff01aaa4284a14
Atacadoweb <atacadowebonline@gmail.com>
2021-06-29 11:15:39 -0300

    Função string reverse sem passar pelo for

--- scripts.js
+++ scripts.js
@@ -5,8 +5,10 @@ let newStr = '';
 function reverseAstring(str){
     for (let i = str.length -1; i >= 0; i--){
         newStr += str[i];
-        console.log(newStr);
-    }
-}
+      
+    } 
+    
+    console.log(newStr); // se eu passar o console para linha de baixo.. não passa pelo for
+} 
 
 let resultado = reverseAstring('Reginaldo')

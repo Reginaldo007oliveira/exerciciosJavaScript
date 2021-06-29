5e9078f8b43f65176eadd7049f84a9df242d1998
Atacadoweb <atacadowebonline@gmail.com>
2021-06-29 20:38:09 -0300

    Desenvolvimento To Do List

--- scripts.js
+++ scripts.js
@@ -1,2 +1,32 @@
+/** capitura os dados do html e traz para o js */
 const listContainer = document.querySelector('[data-lists]')
-console.log(listContainer)
+const newListForm = document.querySelector('[data-new-list-form]')
+const newListImput = document.querySelector('[data-new-list-input]')
+
+let lists = []
+/**  A FUNÇÃO A BAIXO  PEGA O VALOR DE CADA IMPUT E VAI ADD DENTRO NO ARRAY lists*/
+newListForm.addEventListener('submit', function(e){
+    e.preventDefault()
+    const listName = newListImput.value 
+    if (listName === null || listName === '') return
+    const list = createList (listName)
+    newListImput.value = null
+    lists.push (list)
+    render()
+
+})
+
+// capitura os dados do js e joga para o html
+function render(){
+    lists.forEach(function(list){
+        const item = document.createElement('li')
+        item.classList.add('item')
+        item.innerText = list.name
+        listContainer.appendChild(item)
+    })
+}
+function createList(name){
+    return { id: Date.now().toString(), name:name}
+}
+
+render()

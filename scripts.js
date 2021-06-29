3f1742ac5d2cdfd38d2cdfe5b076f235065396de
Atacadoweb <atacadowebonline@gmail.com>
2021-06-29 11:37:42 -0300

    Função que converte Celsius para Fahrenheit

---
+++ scripts.js
@@ -0,0 +1,7 @@
+//convert Celsius to Fahrenheit
+
+function convertToFahrenheit(value){
+    return value * 1.8 + 32
+}
+let result = convertToFahrenheit(380)
+console.log(`O valor em Fahrenheit é ${result}`)

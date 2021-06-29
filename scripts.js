c311b560fdd7f52089e293e962760c864072dfdf
Atacadoweb <atacadowebonline@gmail.com>
2021-06-29 11:12:18 -0300

    Função que imprime um nome ao contrario e faz o decremento das vogais do nome

---
+++ scripts.js
@@ -0,0 +1,12 @@
+//Função Reverse a string
+
+let newStr = '';
+
+function reverseAstring(str){
+    for (let i = str.length -1; i >= 0; i--){
+        newStr += str[i];
+        console.log(newStr);
+    }
+}
+
+let resultado = reverseAstring('Reginaldo')
